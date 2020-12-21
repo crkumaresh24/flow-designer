b5c194ddf334ce43fdc326cecbcaedb6bdc058fb
Kumaresh Ramakrishnan <crkumaresh24@gmail.com>
2020-12-21 13:33:58 +0530

    Added support to read/write from S3 lake

--- src/components/tasks/data-extract/ReadTableTask.tsx
+++ src/components/tasks/data-extract/ReadTableTask.tsx
@@ -1,21 +1,43 @@
-import { Form, Input } from 'antd';
+import { Form, Input, Select } from 'antd';
 import React from 'react';
+import { BACKEND_URL } from '../../../globals';
 import { TasksProps, onFieldsChange } from '../Task';
 
 const ReadTableTask = (props: TasksProps): React.ReactElement => {
+    const [buckets, setBuckets] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch(BACKEND_URL + "/buckets")
+            .then(response => response.json())
+            .then(buckets => setBuckets(buckets));
+    }, []);
+
     return (
         <Form
             name="global_state"
-            layout="inline"
+            layout="vertical"
             onFieldsChange={(changedFields, allFields) => onFieldsChange(changedFields, allFields, props)}
             fields={[
                 {
-                    name: ['tableId'],
-                    value: props.task.request.tableId,
+                    name: ['bucketName'],
+                    value: props.task.request.bucketName,
+                },
+                {
+                    name: ['tableName'],
+                    value: props.task.request.tableName,
                 },
             ]}
         >
-            <Form.Item name="tableId" label="Table Id" rules={[{ required: true, message: 'Table ID is required!' }]}>
+            <Form.Item name="bucketName" label="Bucket" rules={[{ required: true, message: 'Bucket name is required!' }]}>
+                <Select>
+                    {
+                        buckets && buckets.map(bucket => {
+                            return (<Select.Option key={bucket} value={bucket}>{bucket}</Select.Option>)
+                        })
+                    }
+                </Select>
+            </Form.Item>
+            <Form.Item name="tableName" label="Table Name" rules={[{ required: true, message: 'Table Name is required!' }]}>
                 <Input />
             </Form.Item>
         </Form>
